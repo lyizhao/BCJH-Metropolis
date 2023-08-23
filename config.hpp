df6d57b37b8c70e8c79631d88200bfa21b38f491
lyizhao <139618935+lyizhao@users.noreply.github.com>
2023-08-23 15:47:39 +0800

    Update config.hpp

--- config.hpp
+++ config.hpp
@@ -16,7 +16,7 @@
 #define ITER_RECIPE 10000 // 对于每一组厨师，选择菜谱时的迭代次数
 
 #define AVOID_CHEF_1 true
-#define AVOID_CHEF_2 false
+#define AVOID_CHEF_2 true
 #define AVOID_CHEF_3 false // 计算时不考虑三火厨师
 
 // ************************************************** //
